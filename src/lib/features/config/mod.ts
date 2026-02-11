d44a92cef7caec191ba405c159279d52ba349fb1
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2026-02-11 13:36:37 -0600

    fmt scripts

--- src/lib/features/config/mod.ts
+++ src/lib/features/config/mod.ts
@@ -2,13 +2,13 @@
 // Central export point for all config-related modules
 
 // Stores
-export { themeStore, type Theme } from './theme.svelte.ts';
-export { currencyStore, type Currency } from './currency.svelte.ts';
-export { languageStore, type Language } from './language.svelte.ts';
-export { appStore, type AppData } from './app.svelte.ts';
+export { type Theme, themeStore } from "./theme.svelte.ts";
+export { type Currency, currencyStore } from "./currency.svelte.ts";
+export { type Language, languageStore } from "./language.svelte.ts";
+export { type AppData, appStore } from "./app.svelte.ts";
 
 // Components
-export { default as AppSettingSelector } from './AppSettingSelector.svelte';
-export { default as ThemeSelector } from './ThemeSelector.svelte';
-export { default as CurrencySelector } from './CurrencySelector.svelte';
-export { default as LanguageSelector } from './LanguageSelector.svelte';
+export { default as AppSettingSelector } from "./AppSettingSelector.svelte";
+export { default as ThemeSelector } from "./ThemeSelector.svelte";
+export { default as CurrencySelector } from "./CurrencySelector.svelte";
+export { default as LanguageSelector } from "./LanguageSelector.svelte";
