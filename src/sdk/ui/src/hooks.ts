bb292d7b0dc6119e75d8c81c2b26d7ff50fe4e8b
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2026-03-24 00:02:25 -0600

    chore: remove unused UI SDK SvelteKit boilerplate
    Deleted the src/sdk/ui directory to remove prototype SvelteKit files that are no longer required for the current project scope. This cleanup removes unused configuration, routing, and hooks files.

--- src/sdk/ui/src/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { deLocalizeUrl } from "$lib/paraglide/runtime.js";
-import type { Reroute } from "@sveltejs/kit";
-
-export const reroute: Reroute = (request) =>
-  deLocalizeUrl(request.url).pathname;
