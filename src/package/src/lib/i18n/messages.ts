08db25b7fdc470a510b9abce8dc863bd80144318
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2026-04-05 15:09:25 -0600

    refactor: update CI scripts to support multiple workspaces
    Updated the CI task definitions to run type-checking across both the 'package' and 'lab' directories instead of a single location. Added a ts-ignore to the i18n messages re-export to resolve missing module declarations during build processes.

--- src/package/src/lib/i18n/messages.ts
+++ src/package/src/lib/i18n/messages.ts
@@ -1,5 +1,7 @@
+// @ts-ignore: paraglide-js generated files are hidden by .gitignore and missing declarations during type-check
 import * as m from "./paraglide/messages.js";
 
+// re-export all the messages as m
 export { m };
 
 // * Note: This is a re-export of the messages from the paraglide-js package.
